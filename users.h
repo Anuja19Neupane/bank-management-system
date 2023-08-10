07d3e80e34ae069e35d5537b0a1de7fed9d541cd
Anuja Neupane <anujaneupane20@gmail.com>
2023-08-10 22:19:43 +0545

    comment added

--- users.h
+++ users.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <stdbool.h>
+#include <stdbool.h> // to use bool
 #include "utils.h"
 
 #define USER_DB_FILE "./USERS.dat"
@@ -22,12 +22,13 @@ typedef struct User {
 
 } User;
 
+// function declaration to create users
 User create_user(char *name, char *password, char *address, int birth_year,int birth_month,int birth_day,very_long_int phone_number);
          // declaring user structure that will return User 
 
 
 // to print results, for my own ease
-void print_user(User user);
+// void print_user(User user);
 
 // to save user to database
 void save_user_to_database(User user);
@@ -44,7 +45,7 @@ User delete_account_by_account_number(very_long_int account_number);
 //update balance 
 User update_balance(very_long_int phone_number, float amount);
 
-
+// get account number by phone number
 float get_account_balance(very_long_int phone_number);
 
 // sort users by aphabetical order of their username
@@ -52,6 +53,7 @@ void sort_users_by_username(User *users, int num_users);
 
 //check validity of user
 bool validate_user(very_long_int phone_number, const char *user_password);
+
 
 void show_user(User user) ;
 
