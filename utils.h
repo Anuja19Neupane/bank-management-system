22d26d84965461fe8b1e2aaa20a5f3a3071be70b
Anuja Neupane <anujaneupane20@gmail.com>
2023-08-10 22:02:56 +0545

    comment added

--- utils.h
+++ utils.h
@@ -46,20 +46,23 @@ Date get_current_date();
 // print date
 void print_date(Date dt);
 
+// function to get date
 void get_date(Date dt, char* date_string);
 
-void getInputString(char *input, int max_length);
+// void getInputString(char *input, int max_length);
 // void getInputDouble(double *input);
 
-
+// for terminal width and height
 int get_terminal_width();
 int get_terminal_height();
 
+// text ko position ko lagi, text lai horizontal tira kati ra vertical tira kati laney vanney kura ho
 void show_text(char *text, float fraction_x, float fraction_y);
 
 void printArbritaryPosition(char *text, float horizantal_fraction);
 
+// to print text at center
 void printTextAtCenter(char *text);
 
-
+// function declaration to clear buffers
 void clear_input_buffer();
